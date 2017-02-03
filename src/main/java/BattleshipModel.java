25c308fa2059cf6f6f8ea80c85d0c31b5e9ce2ca
millelog <millelog@oregonstate.edu>
2017-02-02 19:36:38 -0800

    Added board functionality and battleshipmodel

---
+++ src/main/java/BattleshipModel.java
@@ -0,0 +1,13 @@
+/**
+ * Created by root on 2/2/17.
+ */
+public class BattleshipModel {
+
+    private Board ai;
+    private Board player;
+
+    public void play(){}
+
+    public boolean gameOver(){}
+
+}
--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,9 +5,15 @@ import java.util.ArrayList;
 
 public class Board {
 
-    private ArrayList<Ship> ships = new ArrayList<Ship>();
-    private ArrayList<Coordinate> hits = new ArrayList<Coordinate>();
-    private ArrayList<Coordinate> misses = new ArrayList<Coordinate>();
+    private ArrayList<Ship> ships;
+    private ArrayList<Coordinate> hits;
+    private ArrayList<Coordinate> misses;
+
+    public Board(){
+        ships = new ArrayList<Ship>();
+        hits = new ArrayList<Coordinate>();
+        misses = new ArrayList<Coordinate>();
+    }
 
 
     public void firedAt(int across, int down){
@@ -29,6 +35,16 @@ public class Board {
             }
         }
         return false;
+    }
+
+    public ArrayList<Coordinate> getAllShips(){
+        ArrayList<Coordinate> occupied = new ArrayList<Coordinate>();
+        for(Ship currentShip: ships) {
+            for(Coordinate currentCord: currentShip.getCoordinates()) {
+                occupied.add(currentCord);
+            }
+        }
+        return occupied;
     }
 
     public ArrayList<Ship> getShips(){
