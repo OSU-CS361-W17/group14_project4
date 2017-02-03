cab78b9300543c7b08b956944eb5d643613715bf
millelog <millelog@oregonstate.edu>
2017-02-02 18:43:55 -0800

    Added the definition of base classes for all to use

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 
 public class Board {
 
-    ArrayList<Ship> ships = new ArrayList<Ship>();
-    ArrayList<Coordinate> hits = new ArrayList<Coordinate>();
-    ArrayList<Coordinate> misses = new ArrayList<Coordinate>();
+    private ArrayList<Ship> ships = new ArrayList<Ship>();
+    private ArrayList<Coordinate> hits = new ArrayList<Coordinate>();
+    private ArrayList<Coordinate> misses = new ArrayList<Coordinate>();
 
 
     public void firedAt(int across, int down){
@@ -23,7 +23,7 @@ public class Board {
     public boolean isHit(Coordinate shot){
         for(Ship ship: ships){
             for(Coordinate location : ship.getCoordinates()){
-                if (location.across == shot.across && location.down == shot.down){
+                if (location.getAcross() == shot.getAcross() && location.getDown() == shot.getDown()){
                     return true;
                 }
             }
@@ -31,5 +31,15 @@ public class Board {
         return false;
     }
 
+    public ArrayList<Ship> getShips(){
+        return ships;
+    }
 
+    public ArrayList<Coordinate> getHits(){
+        return hits;
+    }
+
+    public ArrayList<Coordinate> getMisses(){
+        return misses;
+    }
 }
--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -2,10 +2,18 @@
  * Created by millelog on 2/2/17.
  */
 public class Coordinate {
-    public int across, down;
+    private int across, down;
 
     public Coordinate(int a, int d){
         across=a;
         down=d;
+    }
+
+    public int getAcross(){
+        return across;
+    }
+
+    public int getDown(){
+        return down;
     }
 }
