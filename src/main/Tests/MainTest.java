4e735d3cd8948196f006912e1bfce40adbdfa3b0
r3ypoc <pociusr@oregonstate.edu>
2017-03-03 09:11:45 -0800

    removed comments

--- src/main/Tests/MainTest.java
+++ src/main/Tests/MainTest.java
@@ -8,8 +8,6 @@ class MainTest {
     @Test
     void main() {
         String[] a =  {"/Test/1/2/3","/Something/SomethingElse"};
-//        Main t = new Main(a);
-//        assertNotEquals(null,t);
     }
 
     @Test
