5f18dcc2b6b29542f315cc4d2172ce4fb2fff7f0
Logan H. Miller <millelog@oregonstate.edu>
2017-03-03 18:35:16 -0800

    removed print lines

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -76,15 +76,9 @@ public class Board {
 
     public boolean getIsCivilian(Coordinate loc) {
         ArrayList<Ship> temp = ships;
-        System.out.println("USER COORDINATES:");
-        System.out.println(loc.getAcross());
-        System.out.println(loc.getDown());
 
         for (int i = 0; i < temp.size(); i++) {
             for (int j = 0; j < temp.get(i).getCoordinates().length; j++) {
-                System.out.println("SHIP COORDINATES:");
-                System.out.println(temp.get(i).getCoordinates()[j].getAcross());
-                System.out.println(temp.get(i).getCoordinates()[j].getDown());
                 if (temp.get(i).getCoordinates()[j].getAcross() == loc.getAcross() &&
                         temp.get(i).getCoordinates()[j].getDown() == loc.getDown()) {
                     System.out.println("hello");
